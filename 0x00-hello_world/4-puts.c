a165a99bde19f73445f11b54fef6adb0d4be65f5
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-10 09:02:26 -0700

    vefvddv

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ * Main - Entry point
+ *
+ * Description: A C program that print with ut function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
