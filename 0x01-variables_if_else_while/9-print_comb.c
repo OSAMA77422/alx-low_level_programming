88fa404ef12be432bc2c5626c9934aa3b9b8a9cf
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-11 10:33:34 -0700

    vefvddv

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,10 +14,13 @@ int main(void)
 
 	while (digit <= 9)
 	{
-		putchar(digit);
+		putchar(digit + 48);
 		if (digit != 9)
+		{
 			putchar(',');
 			putchar(' ');
+		}
+		digit++;
 	}
 	putchar('\n');
 	return (0);
