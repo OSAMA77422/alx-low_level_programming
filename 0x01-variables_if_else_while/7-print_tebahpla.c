f95d5de2fe0880db44f3b97402a2cc072800027f
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-11 09:54:39 -0700

    vefvddv

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in reverse.
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while ('z' >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
