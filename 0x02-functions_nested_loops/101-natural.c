e966ac79a488b91bdd71cc2693f3572d1dbf7e7f
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-15 09:47:29 -0700

    vefvddv

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: compute the sum of all multiples of 3 and 5
+ *
+ * Return: alawys 0 (success)
+*/
+
+int main(void)
+{
+int sum, num;
+
+for (num = 0; num < 1024; num++)
+{
+if ((num % 3 == 0) || (num % 5 == 0))
+	sum += num;
+}
+printf("%d\n", sum);
+return (0);
+}
