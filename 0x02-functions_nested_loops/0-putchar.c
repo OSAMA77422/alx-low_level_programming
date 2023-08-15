11c4a3905d4232e823f2941b1f36fb36bb591bfe
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-15 06:17:13 -0700

    vefvddv

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main : Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+
+for (ch = 0; ch < 8; ch++)
+{
+_putchar(str[ch]);
+_putchar('\n'0;
+}
+return (0);
+
+}
