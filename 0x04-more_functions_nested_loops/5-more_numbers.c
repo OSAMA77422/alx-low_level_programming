14e4b7e11fa8a89b95a4573bda5bd114fa68096f
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-17 07:14:19 -0700

    vefvddv

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14 ten times and you
+ *
+ * Return: always 0 (success)
+*/
+
+void more_numbers(void)
+{
+	int num, row, count;
+
+	for (row = 0; row <= 10; row++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			count = num;
+		if (count > 9)
+			{
+			_putchar(1 + 48);
+			count = num % 10;
+			}
+		_putchar(0 + 48);
+		}
+	_putchar('\n');
+	}
+}
