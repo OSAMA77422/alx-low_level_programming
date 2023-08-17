d46d470497875108190c3a1d7e771b365870e780
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-17 08:05:52 -0700

    vefvddv

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_line - print a stright line
+ *
+ * @n: is the number of times the character
+ * should be printed
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x <= n; x++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
