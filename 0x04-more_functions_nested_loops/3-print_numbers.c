43a92545ef789840ae0d0ea6c7b937930fe590c7
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-17 05:51:28 -0700

    vefvddv

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * print number - print 0 - 9
+ * main - Entry point
+ * print_numbers - print 0 - 9
  * only using putchar twice
  *
  * Return: alaways 0 (success)
 */
 
-void print_numbers(void)
+int main(void)
 {
 	int num = 0;
+
 	do {
-		_putchar(num + 48)
+		_putchar(num + 48);
 			num++;
 	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 
-
+return (0);
 }
