f4675bbcf30323a1756816037f1fbc7da66cc541
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-17 07:52:51 -0700

    vefvddv

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,16 @@ void more_numbers(void)
 
 	for (row = 0; row <= 10; row++)
 	{
-	for (num = 0; num <= 14; num++)
-	{
-	count = num;
-	if (count > 9)
-	{
-	_putchar(1 + 48);
-	count = num % 10;
-	}
-	_putchar(0 + 48);
-	}
-	_putchar('\n');
+		for (num = 0; num <= 14; num++)
+		{
+			count = num;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				count = num % 10;
+			}
+			_putchar(0 + 48);
+		}
+		_putchar('\n');
 	}
 }
