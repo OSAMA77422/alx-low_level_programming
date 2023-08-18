9fae5e0d0400c9c19a3534fe4ecbe0504a87eeaa
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-18 07:48:22 -0700

    vefvddv

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using the character.
+ *
+ * @size: is the size of square.
+ *
+ * Return: alawys 0 (success)
+*/
+
+void print_square(int size)
+{
+	int row, co;
+
+	for (row = 1; row <= size; row++)
+	{
+	for (co = 1; co <= size; co++)
+		_putchar('#');
+	_putchar('\n');
+	}
+}
