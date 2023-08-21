8574fe478592c78918ab936b9a2a1ef4e7698dbd
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-21 13:59:21 -0700

    vefvddv

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - put half
+ *
+ * @str: input
+ *
+ * Return: nothing.
+*/
+
+void puts_half(char *str)
+{
+int i, q, c = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+	c++;
+}
+q = (c - 1) / 2;
+for (q; str[q]; q++)
+{
+_putchar(str[q]);
+}
+_putchar('\n');
+}
