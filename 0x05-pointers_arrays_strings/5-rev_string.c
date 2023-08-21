341a2b5d662cd2cb10c3699dc94a79feb2707875
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-21 13:12:31 -0700

    vefvddv

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,18 @@
 
 void rev_string(char *s)
 {
+	int i, j;
+	char count;
 
+	for (i = 0; s[i] != '\n'; i++)
+	       count++;	
+
+	for (i = 0; i < (count / 2); i++)
+	{
+	j = s[i];
+	s[i] = s[count - 1 - i];
+	s[count - 1 - i] = j;
+	}
 
 
 
