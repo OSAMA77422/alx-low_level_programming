00debe6837dd1c805de941fc6026cfaf6aba84c6
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-21 07:57:38 -0700

    vefvddv

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swap value
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return : nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t
+}
