3666d23b9a3fc1e5aa5cbb5904e0574ea68cccf1
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-21 08:08:00 -0700

    vefvddv

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - return tje lengh of string.
+ *
+ * @s: input.
+ *
+ * Return : lengh of string.
+*/
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s != ('\n'); s++)
+		c++;
+
+	return (c);
+
+
+}
