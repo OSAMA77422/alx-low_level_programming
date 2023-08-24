2f180a6ed6cf86f91d35651eccd143c668d57b65
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-24 16:58:42 -0700

    vefvddv

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncat - str
+ *
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int x = strlen(dest);
+
+for (i = 0; i < n && *src != '\0'; i++)
+{
+dest[x + i] = src[i];
+}
+dest[x + i] = '\0';
+return (dest);
+}
