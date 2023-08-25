34066d6d30d426ef29c747c53dfd368008f6f357
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-24 17:40:36 -0700

    vefvddv

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,10 +18,6 @@ for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
-while (i < n)
-{
 dest[i] = '\0';
-i++;
-}
 return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp - vf
+ *
+ * @s1: input
+ * @s2: input
+ *
+ * Return: 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+
+
+}
