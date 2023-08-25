a05fe45615a1db5cb47ae78cf6f0ed269ead4317
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-24 17:59:39 -0700

    vefvddv

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
+int i;
 
-
-
+for (i = 0; s1[i] != '\n' || s2[i] != '\n'; i++)
+{
+if (s1[i] != s2[i])
+{
+if (s1[i] > s2[i])
+	return (s1[i] - s2[i]);
+else if (s1[i] < s2[i])
+	return (s1[i] - s2[i]);
+}
+}
+return (0);
 }
