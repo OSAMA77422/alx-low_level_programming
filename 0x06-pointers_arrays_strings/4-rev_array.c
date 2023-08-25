b7a8915e71b178a696dac64ee154eb1a21621b15
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-24 18:10:09 -0700

    vefvddv

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * void reverse_array - void
+ *
+ * @a: input
+ * @n: input
+ *
+ * Return: fvbd
+*/
+
+void reverse_array(int *a, int n)
+{
+int x, y, z;
+
+for (x = 0; y = (n - 1); x < y; y--; x++)
+{
+z = a[x];
+a[x] = a[y];
+a[y] = z;
+}
+}
