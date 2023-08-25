483e7f1e7a272deeb331d983d6282e8767dcda1b
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-24 18:34:24 -0700

    vefvddv

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - vfd
+ *
+ * @str: string
+ * Return: string
+*/
+
+char *string_toupper(char *str)
+{
+int x;
+
+for (x = 0; str[x] != '\n'; x++)
+{
+if (str[x] >= 97 && str[x] <= 122)
+{
+str[x] = str[x] - 32;
+}
+}
+return (str);
+}
