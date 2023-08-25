90e1b1ae8cacbf3c1762e19417d518de7b443978
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-25 15:12:00 -0700

    vefvddv

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,15 +10,14 @@
 
 void print_number(int n)
 {
-	int n1 = n;
 if (n < 0)
 {
 _putchar('-');
-n1 = -n;
+n = -n;
 }
-if (n1 / 10 != 0)
+if (n / 10 != 0)
 {
-print_number(n1 / 10);
+print_number(n / 10);
 }
-_putchar((n1 % 10) + '0');
+_putchar((n % 10) + '0');
 }
