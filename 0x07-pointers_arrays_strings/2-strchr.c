e78753fa3e08b6de790cc852f04100fa09f522a8
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-28 16:21:15 -0700

    vefvddv

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,14 @@ char *_strchr(char *s, char c)
 {
 int i;
 
-for (i = 0; s[i] >= '\0'; i++)
+while (*s != '\0')
 {
-if (s[i] == c)
-{
+if (*s == c)
+	return (s);
+s++;
+}
 return (s);
-}
-}
+
+
 return ('\0');
 }
