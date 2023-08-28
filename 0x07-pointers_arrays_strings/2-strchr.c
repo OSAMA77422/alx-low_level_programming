c86dda1d6dcc56ab51a6c70205b2db3431a336d3
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-28 15:56:20 -0700

    vefvddv

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strchr - e
+ *
+ * @S: input
+ * @c: input
+ *
+ * Return: s, null if its not found
+*/
+
+char *_strchr(char *s, char c)
+{
+int i;
+char *p;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] = c)
+{
+*p = &s[i];
+return (p);
+}
+}
+return ('\0');
+}
