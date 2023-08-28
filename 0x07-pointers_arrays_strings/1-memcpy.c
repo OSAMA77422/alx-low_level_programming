cc9dd014200844e104b56eb1280008de9bd81725
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-28 15:51:07 -0700

    vefvddv

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memcpy - e
+ *
+ * @dest: input
+ * @src: input
+ *
+ * @n: input byte
+ *
+ * Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+	dest[i] = src[i];
+}
+return (dest);
+}
