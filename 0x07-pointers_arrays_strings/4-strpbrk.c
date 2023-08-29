76cc1efe89d202bb6964f9e10e80c31309952740
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-28 17:32:04 -0700

    vefvddv

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,7 +12,6 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
-char *p;
 
 for (i = 0; s[i] != '\0'; i++)
 {
@@ -20,8 +19,7 @@ for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
-	p = &s[i];
-return (p);
+return (s);
 }
 }
 }
