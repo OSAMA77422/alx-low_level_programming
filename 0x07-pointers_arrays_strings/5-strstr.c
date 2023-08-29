adc1eeb3fec9e2a1a75e20a7e029c8ce6088a506
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-28 18:36:44 -0700

    vefvddv

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,14 +13,14 @@ char *_strstr(char *haystack, char *needle)
 {
 int i; x, j = 0;
 
-	for (i = 0; haystack != 0; i++)
+	for (i = 0; haystack[i] != 0; i++)
 	{
 		for (x = 0; needle[x] = haystack[i]; x++)
 		{
 			j++;
 		}
 	}
-	for (i = j; haystack != 0; i++)
+	for (i = j; haystack[i] != 0; i++)
 		{
 			return (haystack);
 		}	
