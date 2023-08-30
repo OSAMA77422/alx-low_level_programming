2e291b28ffb109c6b326e78c1dc56982b31f12d1
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-30 16:31:39 -0700

    vefvddv

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - vdfv
+ *
+ * @s: vdf
+ * Return: vfv
+*/
+
+void _print_rev_recursion(char *s)
+{
+int x, y = 0;
+
+for (x = 0; s[x] != '\0'; x++)
+{
+y++;
+}
+for (x = y - 1; x >= 0; x--)
+{
+putchar (*s);
+}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,5 @@ int _putchar(char c);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 #endif
