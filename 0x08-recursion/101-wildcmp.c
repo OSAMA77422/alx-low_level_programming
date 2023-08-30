9053476634c74f3eb4810c2a3bcffbb348fe9650
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-30 16:02:33 -0700

    vefvddv

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -61,9 +61,9 @@ return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
 }
 if (!*s1 || !*s2)
 	return (0);
-	if (*s2 == '*')
+if (*s2 == '*')
 {
-s2 = move_past_star(s2);
+s2 = *move_past_star(s2);
 if(!*s2)
 	return(1);
 if(*s1 == *s2)
