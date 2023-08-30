6ae4e1f84b631ac86365746ebdce247f9cd99686
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-30 16:08:03 -0700

    vefvddv

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -51,25 +51,25 @@ int wildcmp(char *s1, char *s2)
 {
 int ret = 0;
 
-if (!*s1 && *s2 == '*' && !*move_past_star(s2))
-	return (1);
-if (*s1 == *s2)
-{
-if (!*s1)
-	return (1);
-return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
-}
-if (!*s1 || !*s2)
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+	if (!*s1)
+		return (1);
+	return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !*s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if(!*s2)
+			return(1);
+		if(*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
 	return (0);
-if (*s2 == '*')
-{
-s2 = move_past_star(s2);
-if(!*s2)
-	return(1);
-if(*s1 == *s2)
-	ret += wildcmp(s1 + 1, s2 + 1);
-ret += inception(s1, s2);
-return (!!ret);
-}
-return (0);
 }
