edaf9b9831ea52882b52e1b49946dee4dc24b598
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-30 17:01:59 -0700

    vefvddv

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - vfd
+ *
+ * @n: vef
+ * Return: vs
+*/
+
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}
+
+/**
+ * square - cfv
+ *
+ * @n: vf
+ * @val: fvefs
+ *
+ * Return: fv
+*/
+
+int square(int n, int val);
+{
+if (val * val == n)
+	return (val);
+else if (val * val < n)
+	return (square(n, val + 1));
+else 
+	return (-1);
+}
