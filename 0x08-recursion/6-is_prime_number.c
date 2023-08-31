2b225435788947302c5cc02c42352f73f95a1683
ELSAFTY <osamaelsafty67@gmail.com>
2023-08-30 17:11:01 -0700

    vefvddv

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime_number - vefvd
+ *
+ * @othrn: vfdv
+ * @n: vfdfv
+ * Return: fdvdf
+*/
+
+int check_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - vfvs
+ * @n: dfv
+ * @othrn: vfdv
+ * Return: vsd
+*/
+
+int check_prime(int n, int othrn)
+{
+if (othrn >= n && n > 1)
+	return (1);
+else if (n % othrn == 0 || n <= 1)
+	return (0);
+else 
+	return (check_prime(n, othrn + 1));
+}
