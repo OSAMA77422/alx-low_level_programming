bbbb30ad60e8cbdd3c62f58954d28b48e5e1c313
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-01 10:11:01 -0700

    vefvddv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,14 +15,11 @@ char *c;
 
 while (argv--)
 {
-for (c = argv[argc]; *c; c++)
-
-if (*c < '0' || *c > '9')
-
-return (printf("Error\n"), 1);
-
-sum += atoi(argv[argc]);
+	for (c = argv[argc]; *c; c++)
+		if (*c < '0' || *c > '9')
+			return (printf("Error\n"), 1);
+	sum += atoi(argv[argc]);
 }
-printf("%d\n", sum);
-return (0);
+	printf("%d\n", sum);
+		return (0);
 }
