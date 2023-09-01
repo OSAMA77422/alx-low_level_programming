d011ccb76a3be67619fd6f841f21be99770a8aac
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-01 09:38:40 -0700

    vefvddv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 
 /**
- * */
+ * main -vdfv
+ * @argc: sdfv
+ * @argv: dfdv
+ * Return: v
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
