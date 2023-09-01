3b437962fc36918f92f49df11b2706fbf6ea69ae
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-01 09:19:24 -0700

    vefvddv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - fv
+ * @argc: vfs
+ * @argv: fvdf
+ * Return:  df
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
