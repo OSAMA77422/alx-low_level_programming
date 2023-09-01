168cd3cd813e5f3b96129cc49cdec22d4429085b
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-01 09:56:49 -0700

    vefvddv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -vdf
+ * @argc: svsv
+ * @argv: vfwe
+ * Return: cs
+*/
+
+int main(int argc, char *argv[])
+{
+int sum;
+
+if (argc == 3)
+{
+sum = atoi(argv[1] * atoi(argv[2]);
+		printf("%d\n", sum);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
