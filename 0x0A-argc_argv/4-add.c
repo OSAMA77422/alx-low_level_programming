c7958bc005ff0ce1afab5e54f308edf7b5d7a210
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-01 10:09:32 -0700

    vefvddv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - vdf
+ * @argc: vf
+ * @argv: vvf
+ * Return :Vwd
+*/
+
+int main(int argc, char *argv[])
+{
+int sum = 0;
+char *c;
+
+while (argv--)
+{
+for (c = argv[argc]; *c; c++)
+
+if (*c < '0' || *c > '9')
+
+return (printf("Error\n"), 1);
+
+sum += atoi(argv[argc]);
+}
+printf("%d\n", sum);
+return (0);
+}
