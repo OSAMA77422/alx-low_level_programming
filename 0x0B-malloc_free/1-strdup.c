cb2705fce91eb8ea6805605ac4a2481cb4b60c98
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-04 16:57:43 -0700

    vefvddv

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strdup - vf
+ * @str: string to copy to new location
+ * Return: cfed
+*/
+
+char *_strdup(char *str)
+{
+	int i, count = 0;
+if (str == NULL)
+	return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+char *c = malloc(count);
+
+if (c == 0)
+	return (NULL);
+
+for (i = 0; c[i] != '\0'; i++)
+	c[i] = str[i];
+return (c);
+}
