4cd094ce73acfd9541954365c0c6e69021ec6243
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-04 18:14:34 -0700

    vefvddv

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * void free_grid - vddv
+ * @grid: vdv
+ * @height: vd
+ * Return: vfd
+*/
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+
+for (; i < height; i++)
+	free(grid[i]);
+free(grid);
+
+}
