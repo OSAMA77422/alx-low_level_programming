00938f10fc460448827a9c755313fb3a08cb7b9e
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-04 17:28:31 -0700

    vefvddv

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,13 @@ char *str_concat(char *s1, char *s2)
 {
 int i, x = 0, y = 0;
 char *s;
-if (s1 == NULL && s2 == NULL)
+if (s1 == NULL)
 {
-	return (NULL);
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
 }
 for (i = 0; s1[i] != '\0'; i++)
 {
