d13b02f3199e4bdc51c1ada283b6bc50247858e2
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-04 17:22:48 -0700

    vefvddv

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *str_concat - fvf
+ * @s1: vf
+ * @s2: vf
+ * Return: vf
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+int i, x = 0, y = 0;
+char *s;
+if (s1 == NULL || s2 == NULL)
+{
+	return (NULL);
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+x++;
+}
+for (i = 0; s[i] != '\0'; i++)
+{
+y++;
+}
+s = malloc(x + y + 1);
+
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+s[i] = s1[i];
+}
+for (i = 0; s[i] != '\0'; i++)
+{
+s[x + i] = s2[i];
+}
+}
