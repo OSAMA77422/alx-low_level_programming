4443a57b2cc4a111dea430da648ad4065eb2d8f8
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-07 11:42:57 -0700

    vefvddv

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - vfd
+ * @b: ffgbb
+ * Return: fdv
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *c;
+
+	c = malloc(b);
+
+if (c == 0)
+	exit(98);
+
+return (m);
+}
