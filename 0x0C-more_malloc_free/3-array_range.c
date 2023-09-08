d1424669f008e3d090859262e6b319713c588b2a
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-07 17:13:33 -0700

    vefvddv

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,12 +13,15 @@ int l, i;
 int *ptr;
 
 if (min > mix)
+{
 	return (NULL);
-
+}
 l = max - min + 1;
 ptr = (sizeof(int) * l);
 if (!ptr)
+{
 	return (NULL);
+}
 for (i = 0; i < len; i++)
 	ptr[i] = min++;
 	return (ptr);
