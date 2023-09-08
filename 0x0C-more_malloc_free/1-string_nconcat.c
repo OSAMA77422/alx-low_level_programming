27af58b270e3a92ddfbb78dc5fe23826cb7be23a
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-07 18:02:04 -0700

    vefvddv

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,30 +11,41 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *str;
-unsigned int i, j, s11;
+unsigned int i = 0, j = 0, k = 0, l = 0;
 
 if (s1 == NULL)
 	s1 = "";
 if (s2 == NULL)
 	s2 = "";
 
-for (s11 = 0; s1[s11] != '\0'; s11++)
+while (s1[i])
+	i++;
+
+while (s2[k])
+	k++;
+
+if (n >= k)
+	l = i + k;
+else 
+	l = i + n;
+
+str = malloc(sizeof(char) * l + 1);
+if (str == NULL)
+	return (NULL);
+
+k = 0
+	while (j < l)
 	{
-		;
-	}
-str = malloc(sizeof (char) * s11 + n + 1);
-	if (str == 0)
+	if (j <= i)
+		str[j] = s1[j];
+
+	if (j >= i)
 	{
-		return (NULL);
+	str[j] = s2[k];
+	k++;
 	}
-for (i = 0; s1[i] != '\0'; i++)
-{
-str[i] == s1[i];
-}
-for (j = 0; s2[j] != '\0'; j++)
-{
-str[s11 + j] = s2[j];
-}
-str[s11 + j] = '\0';
+	j++
+	}
+str[j] = '\0';
 return (str);
 }
