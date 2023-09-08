73e58768a743d342d108a34f277ad23631d50173
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-07 17:11:17 -0700

    vefvddv

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *array_range - fv
+ * @min: bvdf
+ * @max: vdfv
+ * Return: dvs
+*/
+
+int *array_range(int min, int max)
+{
+int l, i;
+int *ptr;
+
+if (min > mix)
+	return (NULL);
+
+l = max - min + 1;
+ptr = (sizeof(int) * l);
+if (!ptr)
+	return (NULL);
+for (i = 0; i < len; i++)
+	ptr[i] = min++;
+	return (ptr);
+}
