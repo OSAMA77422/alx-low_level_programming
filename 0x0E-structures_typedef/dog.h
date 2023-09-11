5ba4fe650b6f69fd5d03b55c8927e98858fbdc9a
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-11 16:45:02 -0700

    vefvddv

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - vf
+ * @name: vd
+ * @age: gvf
+ * @owner: vgd
+ * Description: vfd
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
