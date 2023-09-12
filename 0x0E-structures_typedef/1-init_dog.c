eb5ebcbce693f6635781621f03d29505a3c8fee4
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-11 17:14:13 -0700

    vefvddv

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog -  f
+ * @d: d
+ * @name: bd
+ * @age: fdvv
+ * @owner: fv
+ * Return: fd
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d.name = name;
+d.age = age;
+d.owner = owner;
+}
+}
