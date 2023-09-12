f145837abd17f86f0c2af7ede1ed8a851bff2096
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-11 19:33:36 -0700

    vefvddv

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -75,8 +75,8 @@ free(dog);
 free(dog->name);
 return (NULL);
 }
-dog->name =_strcpy(dog->name, name);
-dog->owner =_strcpy(dog->owner, owner);
+_strcpy(dog->name, name);
+_strcpy(dog->owner, owner);
 dog->age = age;
 
 return (dog);
