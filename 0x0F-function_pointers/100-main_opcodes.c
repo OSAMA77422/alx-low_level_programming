91adc092051f9a613f938ffb684e049f7d42141c
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-13 18:40:46 -0700

    vefvddv

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include "fucnction_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - vf
+ * @argc: vfe
+ * @argv: vfd
+ * Return: always 0
+*/
+
+int main(int argc, char **argv)
+{
+char *p = (char *)main;
+int b;
+
+if (argv != 2)
+{
+printf("Error\n"), exit(1);
+}
+b = atoi(argv[1]);
+if (b < 0)
+{
+printf("Erro\n"), exit(2);
+}
+while (--b)
+{
+printf("%02hhx%s", *p++, b ? " " : "\n");
+}
+return (0);
+}
