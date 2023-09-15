25a37362f97d8f5170d4f02b1992f2b881bedebe
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-15 01:57:56 -0700

    vefvddv

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - vdf
+ * @a: fdf
+ * @b: dfvd
+ * Return: df
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - gfvdf
+ * @a: fd
+ * @b: fd
+ * Return: df
+*/
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - fv
+ * @a: fd
+ * @b: df
+ * Return: df
+*/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - fved
+ * @a: cx
+ * @b: df
+ * Return: dfds
+*/
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - dv
+ * @a: d
+ * @b: df
+ * Return: df
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
