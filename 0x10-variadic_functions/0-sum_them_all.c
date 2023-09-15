64a3c7a92d796a6863743d87227ea2f61a6267a8
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-15 06:04:13 -0700

    vefvddv

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - vefd
+ * @n: fdbvd
+ * @...: dfv
+ * Return: dfv
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i, s = 0;
+
+va_list g;
+
+if (!n)
+	return (0);
+
+va_start(g, n);
+	for (i = 0; i < n; i++)
+	{
+		s += va_arg(g, int);
+	}
+va_end(g);
+return(s);
+}
