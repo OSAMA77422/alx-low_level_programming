1e7108ddd0736eeb86581c0f850213aff8dde109
ELSAFTY <osamaelsafty67@gmail.com>
2023-09-15 07:23:33 -0700

    vefvddv

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - vfd
+ * @separator:
+ * @n: fd
+ * @...: df
+ * Return: df
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+int i = n;
+char *c;
+va_list h;
+
+if (!n)
+{
+printf("\n");
+return;
+}
+va_start(h, n);
+
+while (i--)
+{
+printf("%s%s", (c = va_arg(h, char*)) ? c : "(nil)", i ? (separator ? separator : "") : '\n');
+}
+va_end(h);
+}
